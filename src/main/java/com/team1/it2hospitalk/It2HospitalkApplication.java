d29e96c2fab176da1371e59a6f9fceea8fd28e2a
trinhdvt <trinhvideo123@gmail.com>
2021-10-06 23:29:07 +0700

    initial commit
    Setup project

---
+++ src/main/java/com/team1/it2hospitalk/It2HospitalkApplication.java
@@ -0,0 +1,13 @@
+package com.team1.it2hospitalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It2HospitalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It2HospitalkApplication.class, args);
+    }
+
+}

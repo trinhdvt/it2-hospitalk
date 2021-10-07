f1000b61a81d3f77ea0e81b06ca340bff329a64e
trinhdvt <trinhvideo123@gmail.com>
2021-10-07 20:52:08 +0700

    implement web services config

---
+++ src/main/java/com/team1/it2hospitalk/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.team1.it2hospitalk;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(It2HospitalkApplication.class);
+    }
+}
---
+++ src/main/java/com/team1/it2hospitalk/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.team1.it2hospitalk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api",
+                HandlerTypePredicate.forAnnotation(RestController.class));
+    }
+}

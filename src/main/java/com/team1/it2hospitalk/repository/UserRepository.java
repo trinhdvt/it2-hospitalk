3a911acb894bd3c3119b1fd66af7821e625b11c8
trinhdvt <trinhvideo123@gmail.com>
2021-10-26 21:44:22 +0700

    add User List API

--- src/main/java/com/team1/it2hospitalk/repository/UserRepository.java
+++ src/main/java/com/team1/it2hospitalk/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.team1.it2hospitalk.repository;
 
+import com.team1.it2hospitalk.model.entity.Hospital;
 import com.team1.it2hospitalk.model.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,4 +13,8 @@ public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByUsername(String username);
 
     Page<User> getAllByUsernameIsNull(Pageable pageable);
+
+    Page<User> getAllByUsernameIsNotNull(Pageable pageable);
+
+    Page<User> getAllByWorkHospital(Hospital workHospital, Pageable pageable);
 }
---
+++ src/main/java/com/team1/it2hospitalk/service/impl/UserServicesImpl.java
@@ -0,0 +1,38 @@
+package com.team1.it2hospitalk.service.impl;
+
+import com.team1.it2hospitalk.model.entity.User;
+import com.team1.it2hospitalk.model.response.UserDTO;
+import com.team1.it2hospitalk.repository.HospitalRepository;
+import com.team1.it2hospitalk.repository.UserRepository;
+import com.team1.it2hospitalk.service.IUserServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class UserServicesImpl implements IUserServices {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public List<UserDTO> getListOfAllUser(Pageable pageable) {
+        return userRepository.getAllByUsernameIsNotNull(pageable)
+                .map(UserDTO::toUserDTO)
+                .getContent();
+    }
+
+    @Override
+    public List<UserDTO> getListOfAllUserManageBy(String username, Pageable pageable) {
+        User manager = userRepository.findByUsername(username);
+
+        return userRepository.getAllByWorkHospital(manager.getManageHospital(), pageable)
+                .map(UserDTO::toUserDTO)
+                .getContent();
+    }
+}

4ea6ae73cd989255a15b161e893db23aaa26bfff
trinhdvt <trinhvideo123@gmail.com>
2021-12-18 19:32:41 +0700

    add parameter validation

--- src/main/java/com/team1/it2hospitalk/controller/ChannelMsgController.java
+++ src/main/java/com/team1/it2hospitalk/controller/ChannelMsgController.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotEmpty;
+
 @RestController
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@Validated
 public class ChannelMsgController {
 
     private final IMsgServices msgServices;
@@ -37,7 +41,7 @@ public class ChannelMsgController {
     @PostMapping("/room/{roomID}/message")
     public ResponseEntity<?> postNewMessage(Authentication auth,
                                             @PathVariable int roomID,
-                                            String message) {
+                                            @NotEmpty() String message) {
 
         String username = auth.getName();
 
